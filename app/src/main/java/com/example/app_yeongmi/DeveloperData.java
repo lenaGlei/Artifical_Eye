6d89390e5ac9ca5c017a7bdc66ac482c3aa877b4
Lena <lenagn9@gmail.com>
2024-01-25 09:18:00 +0100

    commitLena6

---
+++ app/src/main/java/com/example/app_yeongmi/DeveloperData.java
@@ -0,0 +1,14 @@
+package com.example.app_yeongmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeveloperData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_data);
+    }
+}
